8ae17eedc5186c8f9e380c5c7d5a10753c2df9ff
Nius <nicholas.p.harrell@gmail.com>
2017-10-27 10:15:09 -0400

    Corrected DBFix.sql
    Corrected reversed SQL script.

--- src/main/resources/DBFix.sql
+++ src/main/resources/DBFix.sql
@@ -1,6 +1,7 @@
-ALTER TABLE ChatShop_listings ADD COLUMN itemName TEXT AFTER enchantments;
-ALTER TABLE ChatShop_transactions ADD COLUMN itemName TEXT AFTER enchantments;
-
 -- If upgrading from pre-2.0.0
 ALTER TABLE ChatShop_listings ADD COLUMN enchantments VARCHAR(30) AFTER price;
 ALTER TABLE ChatShop_transactions ADD COLUMN enchantments VARCHAR(30) AFTER price;
+
+-- If upgrading from pre-2.1.0
+ALTER TABLE ChatShop_listings ADD COLUMN itemName TEXT AFTER enchantments;
+ALTER TABLE ChatShop_transactions ADD COLUMN itemName TEXT AFTER enchantments;
